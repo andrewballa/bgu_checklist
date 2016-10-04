5502b53c86f2d5f6d9f61c5d9e5215e8eca56a07
andrewballa <andrew.e.balla@gmail.com>
2016-10-04 15:16:26 -0500

    test

--- checklist/gridview.php
+++ checklist/gridview.php
@@ -241,7 +241,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' )
             },
             editRow: function (recordId){
                 //console.log(recordId);
-                var record = recordId;//this.gridData[recordId];
+                var record = recordId;//this.gridData[recordId]; added comment
 
                 var fname = record.FirstName!=undefined?record.FirstName:"";
                 var lname = record.LastName!=undefined?record.LastName:"";
